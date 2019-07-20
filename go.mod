5052339b7b760d2f844e51b25157bd9f1f3e9a6d
Andrew Snodgrass <git@pointlander.info>
2019-07-20 13:06:54 -0600

    First commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pointlander/inception
+
+go 1.12
+
+require (
+	github.com/pointlander/gradient v0.0.0-20190719052432-173eb9b05d69
+	gonum.org/v1/plot v0.0.0-20190615073203-9aa86143727f
+)
---
+++ main.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image/color"
+	"math/rand"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+
+	"github.com/pointlander/gradient/tf32"
+)
+
+// Result an experiment result
+type Result struct {
+	Costs     []float32
+	Converged bool
+}
+
+// Statistics aggregation of results
+type Statistics struct {
+	Count     int
+	Converged int
+	Epochs    int
+}
+
+// Aggregate adds the results to the statistics
+func (s *Statistics) Aggregate(result Result) {
+	s.Count++
+	if result.Converged {
+		s.Converged++
+		s.Epochs += len(result.Costs)
+	}
+}
+
+// String generates a string for the statistics
+func (s *Statistics) String() string {
+	return fmt.Sprintf("%f %f", float64(s.Converged)/float64(s.Count), float64(s.Epochs)/float64(s.Converged))
+}
+
+// XORExperiment xor neural network experiment
+func XORExperiment(seed int64, width int, inception, context bool) Result {
+	rnd, costs, converged := rand.New(rand.NewSource(seed)), make([]float32, 0, 1000), false
+	random32 := func(a, b float32) float32 {
+		return (b-a)*rnd.Float32() + a
+	}
+
+	input, output := tf32.NewV(2), tf32.NewV(1)
+	w1, w1a, w1b := tf32.NewV(2, width), tf32.NewV(2, 2), tf32.NewV(2, width)
+	b1, b1a, b1b := tf32.NewV(width), tf32.NewV(width, width), tf32.NewV(width)
+	w2, w2a, w2b := tf32.NewV(width), tf32.NewV(width, width), tf32.NewV(width)
+	b2, b2a, b2b := tf32.NewV(1), tf32.NewV(1), tf32.NewV(1)
+	parameters := []*tf32.V{&w1, &w1a, &w1b, &b1, &b1a, &b1b, &w2, &w2a, &w2b, &b2, &b2a, &b2b}
+	var deltas [][]float32
+	for _, p := range parameters {
+		for i := 0; i < cap(p.X); i++ {
+			p.X = append(p.X, random32(-1, 1))
+		}
+		deltas = append(deltas, make([]float32, len(p.X)))
+	}
+	m1, m2, m1a, m2a := w1.Meta(), w2.Meta(), b1.Meta(), b2.Meta()
+	if inception {
+		m1 = tf32.Add(tf32.Mul(w1a.Meta(), m1), w1b.Meta())
+		m1a = tf32.Add(tf32.Mul(b1a.Meta(), m1a), b1b.Meta())
+		m2 = tf32.Add(tf32.Mul(w2a.Meta(), m2), w2b.Meta())
+		m2a = tf32.Add(tf32.Mul(b2a.Meta(), m2a), b2b.Meta())
+	}
+	l1 := tf32.Sigmoid(tf32.Add(tf32.Mul(m1, input.Meta()), m1a))
+	l2 := tf32.Sigmoid(tf32.Add(tf32.Mul(m2, l1), m2a))
+	cost := tf32.Quadratic(l2, output.Meta())
+
+	type Datum struct {
+		input  []float32
+		output []float32
+		deltas [][]float32
+	}
+	data := [...]Datum{
+		{
+			input:  []float32{0, 0},
+			output: []float32{0},
+		},
+		{
+			input:  []float32{1, 0},
+			output: []float32{1},
+		},
+		{
+			input:  []float32{0, 1},
+			output: []float32{1},
+		},
+		{
+			input:  []float32{1, 1},
+			output: []float32{0},
+		},
+	}
+	table := make([]*Datum, len(data))
+	for i := range data {
+		for _, p := range parameters {
+			data[i].deltas = append(data[i].deltas, make([]float32, len(p.X)))
+		}
+		table[i] = &data[i]
+	}
+
+	alpha, eta := float32(.1), float32(.6)
+	for i := 0; i < 10000; i++ {
+		for i := range table {
+			j := i + rnd.Intn(len(data)-i)
+			table[i], table[j] = table[j], table[i]
+		}
+		total := float32(0.0)
+		for j := range table {
+			for _, p := range parameters {
+				p.Zero()
+			}
+			input.Set(table[j].input)
+			output.Set(table[j].output)
+			total += tf32.Gradient(cost).X[0]
+			deltas := deltas
+			if context {
+				deltas = table[j].deltas
+			}
+			for k, p := range parameters {
+				for l, d := range p.D {
+					deltas[k][l] = alpha*deltas[k][l] - eta*d
+					p.X[l] += deltas[k][l]
+				}
+			}
+		}
+		costs = append(costs, total)
+		if total < .001 {
+			converged = true
+			break
+		}
+	}
+
+	if converged {
+		for i := range data {
+			input.X[0], input.X[1] = data[i].input[0], data[i].input[1]
+			var output tf32.V
+			l2(func(a *tf32.V) {
+				output = *a
+			})
+			if data[i].output[0] == 1 && output.X[0] < .5 {
+				panic(fmt.Sprintf("%v output should be 1 %f %v %v", context, output.X[0], data[i].input, data[i].output))
+			} else if data[i].output[0] == 0 && output.X[0] >= .5 {
+				panic(fmt.Sprintf("%v output should be 0 %f %v %v", context, output.X[0], data[i].input, data[i].output))
+			}
+		}
+	}
+
+	return Result{
+		Costs:     costs,
+		Converged: converged,
+	}
+}
+
+// RunXORRepeatedExperiment runs multiple xor experiments
+func RunXORRepeatedExperiment() {
+	experiment := func(seed int64, inception, context bool, results chan<- Result) {
+		results <- XORExperiment(seed, 3, inception, context)
+	}
+	normalStats, contextStats := Statistics{}, Statistics{}
+	normalResults, contextResults := make(chan Result, 8), make(chan Result, 8)
+	for i := 1; i <= 256; i++ {
+		go experiment(int64(i), false, false, normalResults)
+		go experiment(int64(i), true, false, contextResults)
+	}
+	for normalStats.Count < 256 || contextStats.Count < 256 {
+		select {
+		case result := <-normalResults:
+			normalStats.Aggregate(result)
+		case result := <-contextResults:
+			contextStats.Aggregate(result)
+		}
+	}
+	fmt.Printf("normal: %s\n", normalStats.String())
+	fmt.Printf("inception: %s\n", contextStats.String())
+}
+
+// RunXORExperiment runs an xor experiment once
+func RunXORExperiment(seed int64) {
+	normal := XORExperiment(seed, 3, false, false)
+	context := XORExperiment(seed, 3, true, false)
+
+	pointsNormal := make(plotter.XYs, 0, len(normal.Costs))
+	for i, cost := range normal.Costs {
+		pointsNormal = append(pointsNormal, plotter.XY{X: float64(i), Y: float64(cost)})
+	}
+
+	pointsContext := make(plotter.XYs, 0, len(context.Costs))
+	for i, cost := range context.Costs {
+		pointsContext = append(pointsContext, plotter.XY{X: float64(i), Y: float64(cost)})
+	}
+
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = "epochs"
+	p.X.Label.Text = "time"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(pointsNormal)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	scatter.GlyphStyle.Color = color.RGBA{R: 255, A: 255}
+	p.Add(scatter)
+
+	scatter, err = plotter.NewScatter(pointsContext)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	scatter.GlyphStyle.Color = color.RGBA{B: 255, A: 255}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "cost.png")
+	if err != nil {
+		panic(err)
+	}
+}
+
+var (
+	seed                     = flag.Int64("seed", 9, "the seed to use")
+	runXORExperiment         = flag.Bool("xor", false, "run the xor experiment")
+	runXORRepeatedExperiment = flag.Bool("xorRepeated", false, "run the xor experiment repeatedly")
+)
+
+func main() {
+	flag.Parse()
+
+	if *runXORRepeatedExperiment {
+		RunXORRepeatedExperiment()
+		return
+	}
+
+	if *runXORExperiment {
+		RunXORExperiment(*seed)
+		return
+	}
+
+	flag.Usage()
+}
